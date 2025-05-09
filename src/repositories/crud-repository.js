3846b3c1cf4192d1e4fb09e64a5e67879b7e2677
Aaryan1311 <ayt131102@gmail.com>
2025-05-09 19:14:05 +0530

    Added boilerplate for booking

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,64 @@
+const { Logger } = require('../config');
+const AppError = require('../utils/errors/app-error');
+const { StatusCodes } = require('http-status-codes');
+class CrudRepository{
+    constructor(model){
+        this.model = model;
+    }
+
+    async create(data){
+        const response = await this.model.create(data);
+        return response;
+    }
+
+    async destroy(data){
+        const response = await this.model.destroy({
+            where: {
+                id: data
+            }
+        });
+
+        if(!response){
+            throw new AppError('No data found', StatusCodes.NOT_FOUND);
+        }
+        return response;
+  
+    }
+
+    async get(data){
+        const response = await this.model.findByPk(data);
+        if(!response){
+            throw new AppError('No data found', StatusCodes.NOT_FOUND);
+        }
+        return response; 
+    }
+
+    async getAllWithFilter(filter){
+        const response = await this.model.findAll({
+            where: filter
+        });
+        if(!response){
+            throw new AppError('No data found', StatusCodes.NOT_FOUND);
+        }
+        return response;  
+    }
+
+    async getAll(){
+        const response = await this.model.findAll();
+        return response;  
+    }
+
+    async update(id, data){ // id is the primary key and data is the object to be updated (columns and values)
+        const response = await this.model.update(data, {
+            where: {
+                id: id
+            }
+        });
+        if(response[0] == 0){
+            throw new AppError('No data found', StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+}
+
+module.exports = CrudRepository;
